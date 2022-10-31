d7db05e62e9dd35454ffca117c9c4c8328d88f8a
Marian Goenninger <marian.goenninger@student.uni-tuebingen.de>
2022-10-31 15:33:23 +0100

    getf, call

--- src/mago_gbt/onto/main.clj
+++ src/mago_gbt/onto/main.clj
@@ -1,27 +1,38 @@
 (ns mago-gbt.onto.main)
 
+(defn ogetf [o]
+      (fn [k]
+          ((k o) o)))
+
+(defn ocall [o]
+      (fn [k & args]
+          (apply ((((:ogetf o) o) k) o)
+                 args)))
+
 (defn otransform [o]
-                 (fn [f]
-                     (fn [x]
-                         ((f o) x))))
+      (fn [f]
+          (fn [x]
+              ((f o) x))))
 
-(defn oreduce (fn [o]
-                  (fn ([rf elems]
-                       ((oreduce o) rf
-                                    (first elems)
-                                    (rest elems)))
-                      ([rf acc elems]
-                       (if (not (empty? elems))
-                           ((oreduce o) rf
-                                        (rf acc (first elems))
-                                        (rest elems))
-                           acc)))))
+(defn oreduce [o]
+      (fn ([rf elems]
+           ((oreduce o) rf
+                        (first elems)
+                        (rest elems)))
+          ([rf acc elems]
+           (if (not (empty? elems))
+               ((oreduce o) rf
+                            (rf acc (first elems))
+                            (rest elems))
+               acc))))
 
-(defn oregister (fn [o]
-                    (fn [n f]
-                        (assoc o n f))))
+(defn oregister [o]
+      (fn [n f]
+          (assoc o n f)))
 
 (def base
-     {:transform otransform
+     {:getf ogetf
+      :call ocall
+      :transform otransform
       :reduce oreduce
       :register oregister})
