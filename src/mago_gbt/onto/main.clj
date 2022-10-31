b341793075af119f5d571e94361cd3d4a354b12c
Marian Goenninger <marian.goenninger@student.uni-tuebingen.de>
2022-10-31 14:53:16 +0100

    initial main, monad

--- src/mago_gbt/onto/main.clj
+++ src/mago_gbt/onto/main.clj
@@ -1,1 +1,27 @@
-(ns mago-gbt.mago-gbt.onto.main)
+(ns mago-gbt.onto.main)
+
+(defn otransform [o]
+                 (fn [f]
+                     (fn [x]
+                         ((f o) x))))
+
+(defn oreduce (fn [o]
+                  (fn ([rf elems]
+                       ((oreduce o) rf
+                                    (first elems)
+                                    (rest elems)))
+                      ([rf acc elems]
+                       (if (not (empty? elems))
+                           ((oreduce o) rf
+                                        (rf acc (first elems))
+                                        (rest elems))
+                           acc)))))
+
+(defn oregister (fn [o]
+                    (fn [n f]
+                        (assoc o n f))))
+
+(def base
+     {:transform otransform
+      :reduce oreduce
+      :register oregister})
